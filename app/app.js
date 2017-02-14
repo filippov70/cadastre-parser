1729eab7d009995a6305f3c52d626cb0ac1547f7
filippov70 <filippov70@gmail.com>
2017-02-14 10:26:00 +0700

    неудачно проверл отркрытие большого (500 мб) средствами NodeJS - не открывает. http://stackoverflow.com/questions/40382268/error-tostring-failed-node-js-buffer

--- app/app.js
+++ app/app.js
@@ -4,72 +4,95 @@
  * and open the template in the editor.
  */
 
-    var Converter = require('cadXML2GeoJSON/cadXML2GeoJSON.js');
-    var fs = require('fs');
-    var jsonfile = require('jsonfile');
-    var inputFile = null;
-    var convertedFiles;
-    console.log(Converter);
+var Converter = require('cadXML2GeoJSON/cadXML2GeoJSON.js');
+var fs = require('fs');
+var jsonfile = require('jsonfile');
+var inputFile = null;
+var convertedFiles;
+var app = require('electron').remote;
+var dialog = app.dialog;
+//console.log(Converter);
 
-    $('input[type=file]').on('change', prepareUpload);
-    $('#save-geo').on('click', saveGeoJSON);
+$('#input-file').on('click', loadXml);
+$('#save-geo').on('click', saveGeoJSON);
 
-    function prepareUpload(event) {
-        inputFile = event.target.files[0].path;
-        //console.log(Converter);
-        
-        
-        $.get(inputFile, function (xml) {
-            //var json = $.xml2json(xml).CadastralBlocks;
-            // $("#data").html('<code>'+JSON.stringify(json)+'</code>');
-            parsedData = Converter.GeoJSON(xml, false);
-            console.log(parsedData);
-        }).success(
-                function () {
-                    convertedFiles = parsedData;
-                    console.log(convertedFiles);
-                    $('#save-geo').toggleClass('hidden');
+// открытие средствами NodeJS
+function loadXml() {
+    dialog.showOpenDialog(function (fileNames) {
+        if (fileNames === undefined) {
+            console.log("No file selected");
+            // https://github.com/nodejs/node/pull/4394
+        } else {
+            fs.readFileSync(fileNames[0], 'utf-8', function (err, data) {
+                if (err) {
+                    alert("An error ocurred reading the file :" + err.message);
+                    return;
                 }
-        );
-    };
-    
-    function saveGeoJSON () {
-        jsonfile.writeFile(inputFile+'_Parcels.geojson', parsedData.geoJSONParcels, function(err){
-            if (err){
-                console.log(err);
-                return;
+                parsedData = Converter.GeoJSON(data, false);
+                console.log(parsedData);
+                $('#save-geo').toggleClass('hidden');
+            });
+        }
+    });
+}
+
+// открытие средствами HTML 5
+function prepareUpload(event) {
+    inputFile = event.target.files[0].path;
+    //console.log(Converter);
+
+    $.get(inputFile, function (xml) {
+        //var json = $.xml2json(xml).CadastralBlocks;
+        // $("#data").html('<code>'+JSON.stringify(json)+'</code>');
+        parsedData = Converter.GeoJSON(xml, false);
+        console.log(parsedData);
+    }).success(
+            function () {
+                convertedFiles = parsedData;
+                console.log(convertedFiles);
+                $('#save-geo').toggleClass('hidden');
             }
-        });
-        jsonfile.writeFile(inputFile+'_Bounds.geojson', parsedData.geoJSONBounds, function(err){
-            if (err){
-                console.log(err);
-                return;
-            }
-        });
-        jsonfile.writeFile(inputFile+'_Quarta.geojson', parsedData.geoJSONQuartal, function(err){
-            if (err){
-                console.log(err);
-                return;
-            }
-        });
-        jsonfile.writeFile(inputFile+'_Realty.geojson', parsedData.geoJSONRealty, function(err){
-            if (err){
-                console.log(err);
-                return;
-            }
-        });
-        jsonfile.writeFile(inputFile+'_RealtyCircle.geojson', parsedData.geoJSONRealtyCircle, function(err){
-            if (err){
-                console.log(err);
-                return;
-            }
-        });
-        jsonfile.writeFile(inputFile+'_Zones.geojson', parsedData.geoJSONZones, function(err){
-            if (err){
-                console.log(err);
-                return;
-            }
-        });
-        $('#save-geo').toggleClass('hidden');
-    }
+    );
+}
+;
+
+function saveGeoJSON() {
+    jsonfile.writeFile(inputFile + '_Parcels.geojson', parsedData.geoJSONParcels, function (err) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+    });
+    jsonfile.writeFile(inputFile + '_Bounds.geojson', parsedData.geoJSONBounds, function (err) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+    });
+    jsonfile.writeFile(inputFile + '_Quarta.geojson', parsedData.geoJSONQuartal, function (err) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+    });
+    jsonfile.writeFile(inputFile + '_Realty.geojson', parsedData.geoJSONRealty, function (err) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+    });
+    jsonfile.writeFile(inputFile + '_RealtyCircle.geojson', parsedData.geoJSONRealtyCircle, function (err) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+    });
+    jsonfile.writeFile(inputFile + '_Zones.geojson', parsedData.geoJSONZones, function (err) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+    });
+    $('#save-geo').toggleClass('hidden');
+}
 
--- index.js
+++ index.js
@@ -1,33 +1,39 @@
-var app = require('app');  // Module to control application life.
-var BrowserWindow = require('browser-window');  // Module to create native browser window.
+/* global __dirname */
+const electron = require('electron');
+const app = electron.app;  // Module to control application life.
+const BrowserWindow = electron.BrowserWindow;
+const path = require('path');
+const url = require('url');
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-var mainWindow = null;
-
+var mainWindow;
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
+app.on('window-all-closed', function () {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform != 'darwin') {
+    if (process.platform !== 'darwin') {
         app.quit();
     }
 });
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-app.on('ready', function() {
+app.on('ready', function () {
     // Create the browser window.
     mainWindow = new BrowserWindow({width: 800, height: 650});
 
     // and load the index.html of the app.
-    mainWindow.loadURL('file://' + __dirname + '/app/index.html');
-    mainWindow.webContents.openDevTools();
+    //mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname + '/app/', 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+    //mainWindow.webContents.openDevTools();
 
     mainWindow.show();
 
     // Emitted when the window is closed.
-    mainWindow.on('closed', function() {
+    mainWindow.on('closed', function () {
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
